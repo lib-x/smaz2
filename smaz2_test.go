9efbaf6837a2bef6b3a51b5a233230967394d61e
虫子樱桃 <czyt.go@gmail.com>
2024-01-30 13:02:51 +0800

    first version

---
+++ smaz2_test.go
@@ -0,0 +1,21 @@
+package smaz2
+
+import (
+	"encoding/base64"
+	"testing"
+)
+
+func Test_Compress(t *testing.T) {
+	compress := Compress("this world is beautiful")
+	b64 := base64.StdEncoding.EncodeToString(compress)
+	t.Log(b64)
+}
+
+func Test_Decompress(t *testing.T) {
+	b, err := base64.StdEncoding.DecodeString("BwEHNZYBIM0BYdsCaWb7")
+	if err != nil {
+		t.Fatal(err)
+	}
+	decompress := Decompress(b)
+	t.Log(decompress)
+}
